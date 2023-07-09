52017c8a7c9435bca3e2be5c7711d1efc9eed695
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-07-09 14:17:17 +0700

    sponsor section

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -6,12 +6,13 @@ function Sponsors() {
   return (
     <div className="py-16">
       <div className="sm:max-w-6xl rounded-md pt-8 mx-4 sm:mx-8 md:mx-auto">
-        <div className="w-11/12 sm:w-2/3 mx-auto mb-10">
+        {/* sponsor */}
+        {/* <div className="w-11/12 sm:w-2/3 mx-auto mb-10">
           <h1 className="focus:outline-none xl:text-4xl gravitas-one text-3xl text-center text-gray-800 font-extrabold pt-4">
             OUR SUPPORTED SPONSOR
           </h1>
-        </div>
-        <div className="sm:py-6 px-8 sm:24 flex flex-wrap items-center">
+        </div> */}
+        {/* <div className="sm:py-6 px-8 sm:24 flex flex-wrap items-center">
           <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
             <Image
               className="focus:outline-none w-12 sm:w-16"
@@ -72,7 +73,10 @@ function Sponsors() {
               role="img"
             />
           </div>
-        </div>
+        </div> */}
+        <div className="flex items-center justify-center w-full">
+      <Image src={"/sponsor.png"} className="rounded-md transition duration-300 transform hover:scale-105" width={1500} height={1500} alt="calling out sponsors" />
+      </div>
       </div>
     </div>
   );
