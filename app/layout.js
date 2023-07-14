7b52abfa0796d6c82e759d7c001944bd92cf9594
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-07-14 13:57:27 +0700

    adding seo and web analytics

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,54 @@
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 import ClientComponent from "./ClientComponent";
 import "react-responsive-modal/styles.css";
+
 export const metadata = {
-  title: "Laksamana 2023",
-  description: "Lomba",
+  title: "Laksamana UGM 2023",
+  // description:
+  //   "Laksamana UGM merupakan sebuah rangkaian acara yang dilaksanakan oleh Korps Mahasiswa Komunikasi UGM. Laksamana digelar untuk menyalurkan ide dan mengembangkan potensi siswa siswi SMA/ Sederajat di seluruh Indonesia dalam bidang Ilmu Komunikasi melalui berbagai bentuk kompetisi, baik secara lisan maupun tertulis.",
+  //   canonical:"www.laksamanaugm.com"
+  //   openGraph:{
+  //   type: "website",
+  //         locale: "en_IE",
+  //         url: "https://www.laksamanaugm.com/",
+  //         siteName: "laksamanaugm",
+  //         images: [
+  //           {
+  //             url: "/logo-laksamana.png",
+  //             width: 120,
+  //             height: 120,
+  //             alt: "Thumbnail laksamana",
+  //           },
+  //         ],
+  //       }
+  openGraph: {
+    title: "Laksamana UGM 2023",
+    canonical: '/',
+    description:
+      "Laksamana UGM merupakan sebuah rangkaian acara yang dilaksanakan oleh Korps Mahasiswa Komunikasi UGM. Laksamana digelar untuk menyalurkan ide dan mengembangkan potensi siswa siswi SMA/ Sederajat di seluruh Indonesia dalam bidang Ilmu Komunikasi melalui berbagai bentuk kompetisi, baik secara lisan maupun tertulis.",
+    url: "<https://www.laksamanaugm.com/>",
+    referrer: 'origin-when-cross-origin',
+    keywords: ['Lomba', 'UGM', 'laksamana ugm','lomba kampus','laksamana'],
+    siteName: "Laksamana UGM",
+    images: [
+      {
+        title: "Laksamana UGM 2023",
+        url: "/laksamana-logo.png",
+        alt: "Thumbnail laksamana",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className="bg-lime-300">
-        <ClientComponent>{children}</ClientComponent>
+        <ClientComponent>
+          {children}
+          <Analytics />
+        </ClientComponent>
       </body>
     </html>
   );
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -7,11 +7,11 @@ function Sponsors() {
     <div className="py-16">
       <div className="rounded-md mt-16 md:mx-auto">
         <div className="w-11/12 sm:w-2/3 mx-auto mb-10">
-          <h1 className="focus:outline-none xl:text-7xl gravitas-one text-5xl text-center text-gray-800 genty-demo font-extrabold pt-4">
+          <h1 className="focus:outline-none xl:text-7xl gravitas-one text-5xl text-center text-primary-blue genty-demo font-extrabold pt-4">
             Our Sponsor
           </h1>
         </div>
-        <div className="sm:py-6 px-8 sm:24 flex justify-center items-center">
+        <div className="sm:py-4 px-8 sm:24 flex justify-center items-center">
           <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
             <Image
               className="focus:outline-none w-[200px]"
@@ -36,7 +36,7 @@ function Sponsors() {
         
       </div>
       <div className="w-2/3 m-auto">
-        <div className="rounded-md mt-16 md:mx-auto flex items-center justify-center">
+        <div className="rounded-md mt-4 md:mx-auto flex items-center justify-center">
           <Image 
             src="/calling-out-text.png"
             width={1300}
