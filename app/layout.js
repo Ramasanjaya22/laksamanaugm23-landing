67ef6cbd4ae7010bbd379c60bbdc04cebfe70a8c
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-07-14 14:07:42 +0700

    seo image

--- app/layout.js
+++ app/layout.js
@@ -5,36 +5,21 @@ import "react-responsive-modal/styles.css";
 
 export const metadata = {
   title: "Laksamana UGM 2023",
-  // description:
-  //   "Laksamana UGM merupakan sebuah rangkaian acara yang dilaksanakan oleh Korps Mahasiswa Komunikasi UGM. Laksamana digelar untuk menyalurkan ide dan mengembangkan potensi siswa siswi SMA/ Sederajat di seluruh Indonesia dalam bidang Ilmu Komunikasi melalui berbagai bentuk kompetisi, baik secara lisan maupun tertulis.",
-  //   canonical:"www.laksamanaugm.com"
-  //   openGraph:{
-  //   type: "website",
-  //         locale: "en_IE",
-  //         url: "https://www.laksamanaugm.com/",
-  //         siteName: "laksamanaugm",
-  //         images: [
-  //           {
-  //             url: "/logo-laksamana.png",
-  //             width: 120,
-  //             height: 120,
-  //             alt: "Thumbnail laksamana",
-  //           },
-  //         ],
-  //       }
+  description:
+    "Laksamana UGM merupakan sebuah rangkaian acara yang dilaksanakan oleh Korps Mahasiswa Komunikasi UGM. Laksamana digelar untuk menyalurkan ide dan mengembangkan potensi siswa siswi SMA/ Sederajat di seluruh Indonesia dalam bidang Ilmu Komunikasi melalui berbagai bentuk kompetisi, baik secara lisan maupun tertulis.",
   openGraph: {
     title: "Laksamana UGM 2023",
     canonical: '/',
     description:
       "Laksamana UGM merupakan sebuah rangkaian acara yang dilaksanakan oleh Korps Mahasiswa Komunikasi UGM. Laksamana digelar untuk menyalurkan ide dan mengembangkan potensi siswa siswi SMA/ Sederajat di seluruh Indonesia dalam bidang Ilmu Komunikasi melalui berbagai bentuk kompetisi, baik secara lisan maupun tertulis.",
-    url: "<https://www.laksamanaugm.com/>",
+    url: "https://www.laksamanaugm.com/",
     referrer: 'origin-when-cross-origin',
     keywords: ['Lomba', 'UGM', 'laksamana ugm','lomba kampus','laksamana'],
     siteName: "Laksamana UGM",
     images: [
       {
         title: "Laksamana UGM 2023",
-        url: "/laksamana-logo.png",
+        url: "/logo-laksamana.png",
         alt: "Thumbnail laksamana",
       },
     ],
