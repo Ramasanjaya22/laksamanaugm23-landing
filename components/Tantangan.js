8a3c63f6b7dc7e4030b173d5046a42111794286c
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-07-15 23:55:12 +0700

    fixing tantangan

--- components/Tantangan.js
+++ components/Tantangan.js
@@ -4,40 +4,38 @@ import React from "react";
 
 function Tantangan() {
   return (
-    <div>
-      <div className="flex">
-        <div className="flex justify-center items-center h-screen overflow-hidden">
-          <div className="md:full w-screen flex flex-col justify-center items-center text-[#26223D]">
-            <Image
-              src={"/tantangan_laksamana.svg"}
-              width={1000}
-              height={1000}
-              className="text-center mb-2 px-8"
-              alt="tantangan laksamana"
-            />
-            <div className="flex flex-col justify-center items-center">
-              <Link href={"#matalomba"}>
-                {/* <button class="bg-gradient-to-b from-rose-50 to-yellow-500 hover:to-yellow-400 rounded-[32px] mx-8 helvetica text-md lg:text-3xl text-lime-500 font-bold py-6 lg:py-6 px-10 lg:px-20">
+    <div className="flex">
+      <div className="flex justify-center items-center">
+        <div className="w-screen flex flex-col justify-center items-center text-[#26223D]">
+          <Image
+            src={"/tantangan_laksamana.svg"}
+            width={1000}
+            height={1000}
+            className="text-center my-8 px-4 w-full lg:w-3/4"
+            alt="tantangan laksamana"
+          />
+          <div className="flex flex-col justify-center items-center">
+            <Link href={"#matalomba"}>
+              {/* <button class="bg-gradient-to-b from-rose-50 to-yellow-500 hover:to-yellow-400 rounded-[32px] mx-8 helvetica text-md lg:text-3xl text-lime-500 font-bold py-6 lg:py-6 px-10 lg:px-20">
                   Daftarkan Dirimu Sekarang!
                 </button> */}
-                  <Image
-              src={"/daftarkan-diri.png"}
-              alt="daftarkan diri"
-              width={1000}
-              height={1000}
-              className="w-96 mt-8 snap-animation"
-            />
-              </Link>
-            </div>
-          
-            <Image
-              src={"/siaplaksanakan.png"}
-              alt="siap laksanakan"
-              width={1000}
-              height={1000}
-              className="w-80 mt-11"
-            />
+              <Image
+                src={"/daftarkan-diri.png"}
+                alt="daftarkan diri"
+                width={1000}
+                height={1000}
+                className="w-96 snap-animation"
+              />
+            </Link>
           </div>
+
+          <Image
+            src={"/siaplaksanakan.png"}
+            alt="siap laksanakan"
+            width={1000}
+            height={1000}
+            className="w-80 mt-8"
+          />
         </div>
       </div>
     </div>
